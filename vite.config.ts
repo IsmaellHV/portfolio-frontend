fdc481de436e9b4b14c98486a08a85b530c34d54
“ismaellhv” <ismaelhv@outlook.com>
2025-08-24 00:53:46 -0500

    refactor(vite): remove unused isBuild variable in config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import obfuscatorPlugin from 'rollup-plugin-javascript-obfuscator';
 
-export default defineConfig(({ command }) => {
-  const isBuild = command === 'build';
+export default defineConfig(() => {
   const isProduction = process.env.NODE_ENV === 'production';
 
   return {
